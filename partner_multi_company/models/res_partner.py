d544182e76dc783a0277e3250f52d26635d0dc50
Rod Schouteden <rod.schouteden@dynapps.be>
2024-12-10 09:27:22 +0100

    pre-commit

--- partner_multi_company/models/res_partner.py
+++ partner_multi_company/models/res_partner.py
@@ -2,7 +2,7 @@
 # Copyright 2015-2019 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html.html
 
-from odoo import Command, _, api, fields, models
+from odoo import Command, _, api, models
 from odoo.exceptions import ValidationError
 
 
